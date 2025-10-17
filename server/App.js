2e6895a4619c69ee42e6c3d8aa75ed26fa530a4e
Avanish <singhavanish720@gmail.com>
2025-10-17 23:26:17 +0530

    fix error

--- server/App.js
+++ server/App.js
@@ -7,17 +7,12 @@ import reportRoutes from "./routes/reportRoutes.js";
 
 dotenv.config();
 const app = express();
-// app.use(cors());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://financial-credit-report.vercel.app",
-  })
-);
+app.use(cors());
 
 connectDB();
-app.use("/api/upload", uploadRoutes);
-app.use("/api/reports", reportRoutes);
+app.use("/upload", uploadRoutes);
+app.use("/reports", reportRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
